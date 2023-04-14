d9286a106342abc5b86d5a66838ede6237da8d0d
Sanjit93 <skrath93@gmail.com>
2023-04-14 21:26:16 +0530

    Use some symbolbol

--- MyApps1/views.py
+++ MyApps1/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 #create your views here
 def welcome_view(request):
 	print('This line added by view function names welcome_view...!!!')
-	return HttpResponse('<h1>###Custom Middleware Demo###</h1> <hr />')
-
+	return HttpResponse('<h1>Custom Middleware Demo#</h1> <hr />')
 
 from django.http import HttpResponse
 # Create your views here.
